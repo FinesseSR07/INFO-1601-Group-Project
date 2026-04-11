b0be45222a21b82c28b1e20320863422fad81ba2
FinesseSR07 <saranresal284@gmail.com>
2026-04-11 18:46:13 -0400

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCofdxtXTtLefUUqDsq3vcarnVT0J7kuDM",
+  authDomain: "info-1601-group-project-635e4.firebaseapp.com",
+  projectId: "info-1601-group-project-635e4",
+  storageBucket: "info-1601-group-project-635e4.firebasestorage.app",
+  messagingSenderId: "103724767935",
+  appId: "1:103724767935:web:7f91c37b6c76eacf61840a",
+  measurementId: "G-BK9ZGGKRF6"
+};
+
+export default firebaseConfig
