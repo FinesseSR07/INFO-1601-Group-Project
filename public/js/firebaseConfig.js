9b4e824dd95be262e6e4687752a7265141ffa5e2
FinesseSR07 <saranresal284@gmail.com>
2026-04-11 22:17:14 -0400

    deploy fixing

--- public/js/firebaseConfig.js
+++ public/js/firebaseConfig.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
   apiKey: "AIzaSyBwRJa-c2wz911yKX7oSGpvwmEzBMGr-4w",
   authDomain: "info1601project-d6470.firebaseapp.com",
-  projectId: "info1601project-d6470",
-  storageBucket: "info1601project-d6470.firebasestorage.app",
+  projectId: "info-1601-group-project-635e4",
+  storageBucket: "info-1601-group-project-635e4.firebasestorage.app",
   messagingSenderId: "420607635079",
   appId: "1:420607635079:web:9dd278536c37be5f0f5222"
 };
